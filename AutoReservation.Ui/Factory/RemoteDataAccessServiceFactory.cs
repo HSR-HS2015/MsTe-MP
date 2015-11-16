9e030c1c2f09e9ff3c63b169ff8d455c00c14ef4
Fabian Binna <fbinna@hsr.ch>
2015-11-16 14:17:41 +0100

    RemoteDataAccessServiceFactory

---
+++ AutoReservation.Ui/Factory/RemoteDataAccessServiceFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoReservation.Common.Interfaces;
+using System.ServiceModel;
+
+namespace AutoReservation.Ui.Factory
+{
+    class RemoteDataAccessServiceFactory : IServiceFactory
+    {
+        public IAutoReservationService GetService()
+        {
+            ChannelFactory<IAutoReservationService> myChannelFactory = new ChannelFactory<IAutoReservationService>("AutoReservationService");
+
+            // Create a channel.
+            return myChannelFactory.CreateChannel();
+        }
+    }
+}
